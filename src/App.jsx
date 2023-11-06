d3d9e571bf9b77dce326516bda08ce53facd7d17
Huseyin <kucukalanhuseyin@gmail.com>
2023-11-06 17:00:50 +0300

    commit edildi

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import StoreNavigation from "./components/StoreNavigation"
+import PromoSection from "./components/PromoSection"
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <StoreNavigation></StoreNavigation>
+      <PromoSection></PromoSection>
+    </div>
+  );
+}
+
+export default App;
